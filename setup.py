88d8466799addd327b281c4b0fd95322f8802870
Santosh Chapgain <santoahsharam98@gmail.com>
2026-06-20 22:29:15 +0545

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Santosh Chapagain",
+    author_email="santosh.chapagain.datasci@gmail.com",
+    packages=find_packages()
+)
